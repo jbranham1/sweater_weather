20eb6594b8bea07a365b61707ca750a69e67fc48
Jenny Branham <branhamjenny@gmail.com>
2021-04-25 13:10:28 -0500

    Add route to mapquest service

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -4,7 +4,20 @@ class MapquestService
       key: ENV['MAPQUEST_KEY'],
       location: location
     }
-    response = connection.get("address?") do |req|
+    response = connection.get("/geocoding/v1/address?") do |req|
+      req.headers["CONTENT_TYPE"] = "application/json"
+      req.params = location_params
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.find_directions(origin, destination)
+    location_params = {
+      key: ENV['MAPQUEST_KEY'],
+      from: origin,
+      to: destination
+    }
+    response = connection.get("/directions/v2/route?") do |req|
       req.headers["CONTENT_TYPE"] = "application/json"
       req.params = location_params
     end
@@ -12,6 +25,6 @@ class MapquestService
   end
 
   def self.connection
-    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/')
+    Faraday.new(url: 'http://www.mapquestapi.com')
   end
 end
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,42 +1,118 @@
 require 'rails_helper'
 
 RSpec.describe "Mapquest Search" do
-  it "returns the latitude and longitude of a city" do
-    VCR.use_cassette("mapquest_service") do
-      location = "denver, co"
-      response = MapquestService.find_coordinal_location(location)
-
-      expect(response).to be_a Hash
-      expect(response).to have_key(:info)
-      expect(response).to have_key(:results)
-      results = response[:results]
-
-      expect(results).to be_an Array
-      results1 = results.first
-      expect(results1).to be_a Hash
-      expect(results1).to have_key(:locations)
-      expect(results1[:locations]).to be_an Array
-      location = results1[:locations].first
-
-      expect(location).to have_key(:latLng)
-      expect(location[:latLng]).to be_a Hash
-      expect(location[:latLng]).to have_key(:lat)
-      expect(location[:latLng]).to have_key(:lng)
-    end
-  end
-  describe "sad path" do
-    it "returns a 400 error if location is empty" do
-      VCR.use_cassette("mapquest_service_empty_location") do
-        location = ""
+  describe "find coordinal location" do
+    it "returns the latitude and longitude of a city" do
+      VCR.use_cassette("mapquest_service") do
+        location = "denver, co"
         response = MapquestService.find_coordinal_location(location)
 
         expect(response).to be_a Hash
         expect(response).to have_key(:info)
+        expect(response).to have_key(:results)
+        results = response[:results]
 
-        expect(response[:info]).to have_key(:statuscode)
-        expect(response[:info][:statuscode]).to eq(400)
-        expect(response[:info]).to have_key(:messages)
-        expect(response[:info][:messages]).to eq(["Illegal argument from request: Insufficient info for location"])
+        expect(results).to be_an Array
+        results1 = results.first
+        expect(results1).to be_a Hash
+        expect(results1).to have_key(:locations)
+        expect(results1[:locations]).to be_an Array
+        location = results1[:locations].first
+
+        expect(location).to have_key(:latLng)
+        expect(location[:latLng]).to be_a Hash
+        expect(location[:latLng]).to have_key(:lat)
+        expect(location[:latLng]).to have_key(:lng)
+      end
+    end
+    describe "sad path" do
+      it "returns a 400 error if location is empty" do
+        VCR.use_cassette("mapquest_service_empty_location") do
+          location = ""
+          response = MapquestService.find_coordinal_location(location)
+
+          expect(response).to be_a Hash
+          expect(response).to have_key(:info)
+
+          expect(response[:info]).to have_key(:statuscode)
+          expect(response[:info][:statuscode]).to eq(400)
+          expect(response[:info]).to have_key(:messages)
+          expect(response[:info][:messages]).to eq(["Illegal argument from request: Insufficient info for location"])
+        end
+      end
+    end
+  end
+  describe "find route" do
+    it "returns the route of a trip when given origin and destination locations" do
+      VCR.use_cassette("mapquest_route_service") do
+        origin = "conway,ar"
+        destination = "denver,co"
+        response = MapquestService.find_directions(origin, destination)
+
+        expect(response).to be_a Hash
+        expect(response).to have_key(:route)
+        route = response[:route]
+
+        expect(route).to be_a Hash
+        expect(route).to have_key(:hasTollRoad)
+        expect(route).to have_key(:boundingBox)
+        expect(route[:boundingBox]).to be_a Hash
+        expect(route).to have_key(:distance)
+        expect(route[:distance]).to be_a Float
+        expect(route).to have_key(:hasTimedRestriction)
+        expect(route).to have_key(:hasTunnel)
+        expect(route).to have_key(:hasHighway)
+        expect(route).to have_key(:formattedTime)
+        expect(route[:formattedTime]).to be_a String
+        expect(route).to have_key(:realTime)
+        expect(route[:realTime]).to be_a Integer
+      end
+    end
+    describe "sad path" do
+      it "returns an error if origin is empty" do
+        VCR.use_cassette("mapquest_route_service_empty_origin") do
+          origin = ""
+          destination = "denver,co"
+          response = MapquestService.find_directions(origin, destination)
+
+          expect(response).to be_a Hash
+          expect(response).to have_key(:info)
+
+          expect(response[:info]).to have_key(:statuscode)
+          expect(response[:info][:statuscode]).to eq(611)
+          expect(response[:info]).to have_key(:messages)
+          expect(response[:info][:messages]).to eq(["At least two locations must be provided."])
+        end
+      end
+      it "returns an error if destination is empty" do
+        VCR.use_cassette("mapquest_route_service_empty_destination") do
+          origin = ""
+          destination = ""
+          response = MapquestService.find_directions(origin, destination)
+
+          expect(response).to be_a Hash
+          expect(response).to have_key(:info)
+
+          expect(response[:info]).to have_key(:statuscode)
+          expect(response[:info][:statuscode]).to eq(611)
+          expect(response[:info]).to have_key(:messages)
+          expect(response[:info][:messages]).to eq(["At least two locations must be provided."])
+        end
+      end
+      it "returns an error if destination is overseas" do
+        VCR.use_cassette("mapquest_route_service_impossible destination") do
+          origin = "conway,ar"
+          destination = "london,uk"
+          response = MapquestService.find_directions(origin, destination)
+
+          expect(response).to be_a Hash
+          expect(response).to have_key(:info)
+
+          expect(response[:info]).to have_key(:statuscode)
+          expect(response[:info][:statuscode]).to eq(402)
+          expect(response[:info]).to have_key(:messages)
+          expect(response[:info][:messages]).to eq(["We are unable to route with the given locations."])
+        end
       end
     end
   end
