194ec006658106aa925236d1ed0e60d8404ecea5
Jenny Branham <branhamjenny@gmail.com>
2021-04-26 10:18:19 -0500

    Initial Request spec

---
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Salaries Search'do
+  describe 'happy path' do
+    it 'can return certain salaries an current weather of a location' do
+      location = 'denver'
+      get "/api/v1/salaries?destination=#{location}"
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      salaries = JSON.parse(response.body, symbolize_names:true)
+
+      expect(salaries).to be_a(Hash)
+      expect(salaries[:data]).to be_a(Hash)
+      expect(salaries[:data].count).to eq(3)
+      expect(salaries[:data][:id].nil?).to eq true
+      expect(salaries[:data][:type]).to eq("salaries")
+      expect(salaries[:data][:attributes].count).to eq(3)
+
+      data = salaries[:data][:attributes]
+
+      expect(data).to have_key(:destination)
+      expect(data[:destination]).to be_a String
+      expect(data).to have_key(:forecast)
+      expect(data[:forecast]).to be_a Hash
+
+      forecast = data[:forecast]
+      expect(forecast).to have_key(:summary)
+      expect(forecast[:summary]).to be_a String
+      expect(forecast).to have_key(:temperature)
+      expect(temperature[:forecast]).to be_a String
+
+      expect(data).to have_key(:salaries)
+      expect(data[:salaries]).to be_an Array
+      expect(data[:salaries].count).to eq(7)
+      salary1 = data[:salaries].first
+      expect(salary1).to have_key(:title)
+      expect(salary1[:title]).to be_a String
+      expect(salary1).to have_key(:min)
+      expect(salary1[:min]).to be_a String
+      expect(salary1).to have_key(:max)
+      expect(salary1[:max]).to be_a String
+    end
+  end
+end
