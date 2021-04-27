e83049756bb5cf0b5a91024963ea5f6cc1e585b2
Jenny Branham <branhamjenny@gmail.com>
2021-04-26 19:09:22 -0500

    Add test to facade

--- spec/facades/roadtrip_facade_spec.rb
+++ spec/facades/roadtrip_facade_spec.rb
@@ -21,5 +21,40 @@ RSpec.describe 'Roadtrip Facade'do
         expect(trip.weather_at_eta[:conditions]).to be_a String
       end
     end
+    it 'can return the impossible route for a origin and destination' do
+      VCR.use_cassette "roadtrip_overseas" do
+        origin = "Denver,CO"
+        destination = "London,UK"
+
+        trip = RoadTripFacade.get_route(origin, destination)
+
+        expect(trip).to be_a(Roadtrip)
+        expect(trip.start_city).to be_a String
+        expect(trip.end_city).to be_a String
+        expect(trip.travel_time).to be_a String
+        expect(trip.travel_time).to eq("impossible route")
+        expect(trip.weather_at_eta).to be_a Hash
+        expect(trip.weather_at_eta.empty?).to be true
+      end
+    end
+    it 'can return the long route for a origin and destination' do
+      VCR.use_cassette "roadtrip-long" do
+        origin = "Seattle, WA"
+        destination = "Key West, FL"
+
+        trip = RoadTripFacade.get_route(origin, destination)
+
+        expect(trip).to be_a(Roadtrip)
+        expect(trip.start_city).to be_a String
+        expect(trip.end_city).to be_a String
+        expect(trip.travel_time).to be_a String
+        expect(trip.weather_at_eta).to be_a Hash
+
+        expect(trip.weather_at_eta).to have_key(:temperature)
+        expect(trip.weather_at_eta[:temperature]).to be_a Float
+        expect(trip.weather_at_eta).to have_key(:conditions)
+        expect(trip.weather_at_eta[:conditions]).to be_a String
+      end
+    end
   end
 end
